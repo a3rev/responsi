e7845b155ca7956f6e2747c6b1b76d99a51c58ee
a3rev <mrkunau@gmail.com>
2020-03-25 13:31:00 +0700

    Unit Test

--- functions.php
+++ functions.php
@@ -6,6 +6,13 @@
 
 define( 'RESPONSI_FRAMEWORK_VERSION', '7.8.0' );
 
+if( !defined('STYLESHEETPATH') ){
+	define( 'STYLESHEETPATH', get_stylesheet_directory() );
+}
+if( !defined('TEMPLATEPATH') ){
+	define( 'TEMPLATEPATH', get_template_directory() );
+}
+
 require_once ( 'functions/admin-init.php' );					// Framework Init
 
 /*-----------------------------------------------------------------------------------*/
