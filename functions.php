54693b8d0fac98e97ded4354c5b388be6990efd9
a3rev <mrkunau@gmail.com>
2020-04-03 15:26:52 +0700

    Release Version 7.8.2
    = 7.8.2 - 2020/04/03 =
    * This maintenance release fixes and issue with the WP Admin bar
    * Fix -  .responsi-toolbar z-index over riding the WP Admin bar dropdown menus in layout.css

--- functions.php
+++ functions.php
@@ -4,7 +4,7 @@
 /* ResponsiFramework */
 /*-----------------------------------------------------------------------------------*/
 
-define( 'RESPONSI_FRAMEWORK_VERSION', '7.8.1' );
+define( 'RESPONSI_FRAMEWORK_VERSION', '7.8.2' );
 
 require_once ( 'functions/admin-init.php' );					// Framework Init
 
